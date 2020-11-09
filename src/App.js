e812d667ef94152e2858d6b53bf5c512430c2923
Adriano Grimolizzi <a.grimolizzi@gmail.com>
2020-11-09 10:51:19 +0100

    add form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { value: '' }
+  }
+
+  handleChange = (e) => {
+    this.setState({ value: e.target.value })
+  }
+
+  handleSubmit = (e) => {
+    alert('A keyword has been submitted: ' + this.state.value)
+    e.preventDefault()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>SenTwitter</h1>
+        <h2>Sentiment analysis on Twitter!</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Insert the keyword:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="SUBMIT!" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
